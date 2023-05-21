52e8ace62996e60138b4b5acd230e069ad46d5fd
Ashikur Rahman <ashikmoinkhan@gmail.com>
2023-05-21 18:26:40 +0600

    added Post Toys API

--- index.js
+++ index.js
@@ -72,6 +72,8 @@ async function run() {
         }
       });
 
+      
+
       app.get('/myToys', async (req, res) => {
         console.log(req.query.email);
         let query = {};
@@ -136,7 +138,7 @@ async function run() {
    
 
 
-    
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
